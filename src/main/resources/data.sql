a3e337a02db1da9adf87b68bfc8d7906c63e7287
younbihn <116636919+younbihn@users.noreply.github.com>
2023-11-21 21:25:28 +0900

    test sql
    test sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO "user" (email, name, password, nickname, auth_provider, role, phone, profile_image_url, failed_count, created_date, delete_date) VALUES
+('user1@example.com', 'User One', 'password1', 'nickname1', NULL, 'USER', '010-1234-5671', 'http://example.com/profile1.jpg', 0, NOW(), NULL),
+('user2@example.com', 'User Two', 'password2', 'nickname2', NULL, 'USER', '010-1234-5672', 'http://example.com/profile2.jpg', 0, NOW(), NULL),
+('user3@example.com', 'User Three', 'password3', 'nickname3', NULL, 'USER', '010-1234-5673', 'http://example.com/profile3.jpg', 0, NOW(), NULL),
+('user4@example.com', 'User Four', 'password4', 'nickname4', NULL, 'USER', '010-1234-5674', 'http://example.com/profile4.jpg', 0, NOW(), NULL),
+('user5@example.com', 'User Five', 'password5', 'nickname5', NULL, 'USER', '010-1234-5675', 'http://example.com/profile5.jpg', 0, NOW(), NULL),
+('user6@example.com', 'User Six', 'password6', 'nickname6', NULL, 'USER', '010-1234-5676', 'http://example.com/profile6.jpg', 0, NOW(), NULL),
+('user7@example.com', 'User Seven', 'password7', 'nickname7', NULL, 'ROLEUSER_USER', '010-1234-5677', 'http://example.com/profile7.jpg', 0, NOW(), NULL),
+('user8@example.com', 'User Eight', 'password8', 'nickname8', NULL, 'USER', '010-1234-5678', 'http://example.com/profile8.jpg', 0, NOW(), NULL),
+('user9@example.com', 'User Nine', 'password9', 'nickname9', NULL, 'USER', '010-1234-5679', 'http://example.com/profile9.jpg', 0, NOW(), NULL),
+('user10@example.com', 'User Ten', 'password10', 'nickname10', NULL, 'USER', '010-1234-5680', 'http://example.com/profile10.jpg', 0, NOW(), NULL);
