1553f32ebb6c8088c4a94b60ecbfac8666f457f1
jexnjeux <zieun.na@gmail.com>
2023-11-24 06:58:23 +0900

    chore: 스터디 게시판 controller 주석 추가

--- src/main/java/com/zerobase/munbanggu/studyboard/controller/StudyBoardController.java
+++ src/main/java/com/zerobase/munbanggu/studyboard/controller/StudyBoardController.java
@@ -38,6 +38,39 @@ public class StudyBoardController {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
 
+    /**
+     * 게시글 생성 - type이 `VOTE`인 경우에만 `vote` 보냄
+     * @param id studyId
+     * @param request
+     * {
+     *   "type": NOTICE/GENERAL/VOTE 중 하나
+     *   "title": String
+     *   "userId": Long
+     *   "content": String
+     *   "vote": {
+     *     "title": String
+     *     "options": [
+     *       {
+     *         "optionText": String
+     *       },
+     *       {
+     *         "optionText": String
+     *       },
+     *     ],
+     *     "endDate": LocalDateTime
+     *   }
+     * }
+     * @param result
+     * {
+     *     id: Long,
+     *     title: String,
+     *     userId: Long,
+     *     nickname: String,
+     *     createdDate: LocalDateTime
+     * }
+     * @param authHeader Bearer 방식 JWT
+     * @return
+     */
     @PostMapping("/{study_id}/post")
     public ResponseEntity<PostResponse> create(@PathVariable("study_id") Long id,
             @Valid @RequestBody PostRequest request, BindingResult result,
@@ -62,6 +95,40 @@ public class StudyBoardController {
         return errorMap;
     }
 
+    /**
+     *
+     * @param studyId 스터디 아이디
+     * @param postId 게시글 아이디
+     * @param request
+     * {
+     *   "type": NOTICE/GENERAL/VOTE 중 하나
+     *   "title": String
+     *   "userId": Long
+     *   "content": String
+     *   "vote": {
+     *     "title": String
+     *     "options": [
+     *       {
+     *         "optionText": String
+     *       },
+     *       {
+     *         "optionText": String
+     *       },
+     *     ],
+     *     "endDate": LocalDateTime
+     *   }
+     * }
+     * @param result
+     * {
+     *     id: Long,
+     *     title: String,
+     *     userId: Long,
+     *     nickname: String,
+     *     createdDate: LocalDateTime
+     * }
+     * @param authHeader Bearer 방식 JWT
+     * @return
+     */
     @PutMapping("/{study_id}/post/{post_id}")
     public ResponseEntity<PostResponse> update(@PathVariable("study_id") Long studyId, @PathVariable("post_id") Long postId,
             @Valid @RequestBody PostRequest request, BindingResult result,
@@ -76,6 +143,13 @@ public class StudyBoardController {
         return ResponseEntity.ok().body(studyBoardService.update(request, studyId, postId, token));
     }
 
+    /**
+     * 게시글 삭제
+     * @param studyId 스터디 아이디
+     * @param postId 게시글 아이디
+     * @param authHeader Bearer 방식 JWT
+     * @return String
+     */
     @DeleteMapping("/{study_id}/post/{post_id}")
     public ResponseEntity<String> delete(@PathVariable("study_id") Long studyId, @PathVariable("post_id") Long postId,
             @RequestHeader(value = AUTHORIZATION_HEADER) String authHeader) {
@@ -86,9 +160,34 @@ public class StudyBoardController {
 
     }
 
+    /**
+     * 스터디 게시판 게시글 조회
+     * @param studyId 스터디 아이디
+     * @param keyword 검색어
+     * @param pageable 기본 size 20
+     * @param authHeader Bearer 방식 JWT
+     * @return
+     * {
+     * content: [
+     *  {
+     *      id: Long,
+     *      title: string,
+     *      userId: Long,
+     *      nickname: String,
+     *      createdDate: LocalDateTime
+     *  }
+     *  ],
+     *  pageNumber: int, 현재 페이지
+     *  pageSize: int, 반환된 게시글 수(limit)
+     *  totalPages: int 전체 페이지
+     *  first: boolean 현재 페이지가 첫번째 페이지이면 true
+     *  last: boolean 현재 페이지가 마지막 페이지이면 true
+     *  empty: boolean 게시글 없으면 true
+     *  }
+     */
     @GetMapping("/{study_id}/post")
     public ResponseEntity<PageResponse<PostResponse>> search(@PathVariable("study_id") Long studyId,
-            @RequestParam(value = "keyword") String keyword, @PageableDefault() Pageable pageable,
+            @RequestParam(value = "keyword") String keyword, @PageableDefault(size = 20) Pageable pageable,
             @RequestHeader(value = AUTHORIZATION_HEADER) String authHeader) {
         String token = tokenProvider.getRawToken(authHeader);
 
--- src/main/java/com/zerobase/munbanggu/studyboard/controller/VoteController.java
+++ src/main/java/com/zerobase/munbanggu/studyboard/controller/VoteController.java
@@ -19,11 +19,19 @@ public class VoteController {
     private final VoteService voteService;
     private final TokenProvider tokenProvider;
 
+    /**
+     * 투표 (하나의 투표는 한 번만 가능, 완료 후 재시도할 경우 예외 발생)
+     * @param studyId 스터디 아이디
+     * @param voteId 게시글 내 생성된 투표 아이디 (게시글 생성 시 반환되는 값)
+     * @param optionId 투표 내 선택 항목 아이디 (게시글 생성 시 반환되는 값)
+     * @param authHeader Bearer 방식 JWT
+     * @return
+     */
     @PostMapping("/{study_id}/vote/{vote_id}/vote")
-    public ResponseEntity<?> vote(@PathVariable("study_id") Long studyId, @PathVariable("vote_id") Long voteId,
+    public ResponseEntity<String> vote(@PathVariable("study_id") Long studyId, @PathVariable("vote_id") Long voteId,
             @RequestParam("option") Long optionId, @RequestHeader(name = "Authorization") String authHeader) {
         String token = tokenProvider.getRawToken(authHeader);
         voteService.vote(studyId, voteId, optionId, token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body("투표되었습니다.");
     }
 }
