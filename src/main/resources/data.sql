0dab8db744c93f579af9fb62b889dab85d6729dd
younbihn <younbihn@naver.com>
2023-11-27 23:31:51 +0900

    목데이터 적용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,11 @@ INSERT INTO USER (email, name, password, nickname, auth_provider, role, phone, p
 ('user8@example.com', 'User Eight', 'password8', 'nickname8', NULL, 'USER', '010-1234-5678', 'http://example.com/profile8.jpg', 0, NOW(), NULL),
 ('user9@example.com', 'User Nine', 'password9', 'nickname9', NULL, 'USER', '010-1234-5679', 'http://example.com/profile9.jpg', 0, NOW(), NULL),
 ('user10@example.com', 'User Ten', 'password10', 'nickname10', NULL, 'USER', '010-1234-5680', 'http://example.com/profile10.jpg', 0, NOW(), NULL);
+
+INSERT INTO community (user_id, title, content, photo_imgurl, community_category, view, created_date, modified_date)
+VALUES
+(1, '커뮤니티 제목 1', '커뮤니티 내용 1', 'https://example.com/photo1.jpg', '카테고리1', 100, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, '커뮤니티 제목 2', '커뮤니티 내용 2', 'https://example.com/photo2.jpg', '카테고리2', 150, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3, '커뮤니티 제목 3', '커뮤니티 내용 3', 'https://example.com/photo3.jpg', '카테고리3', 200, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, '커뮤니티 제목 4', '커뮤니티 내용 4', 'https://example.com/photo4.jpg', '카테고리4', 250, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(5, '커뮤니티 제목 5', '커뮤니티 내용 5', 'https://example.com/photo5.jpg', '카테고리5', 300, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
